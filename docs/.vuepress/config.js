9e5631d0ec05d445792ae46d2b64e9bc7170cada
田文高 <twg1920@163.com>
2019-10-19 18:18:37 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+ 
+module.exports = {
+    title: '个人主页',
+    description: '我的博客',
+ 
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            // { text: '博文',
+            //   items: [
+            //     { text: 'Android', link: '/android/' },
+            //     { text: 'ios', link: '/ios/' },
+            //     { text: 'Web', link: '/web/' }
+            //   ] 
+            // },
+            // { text: '关于', link: '/about/' },
+            // { text: 'Github', link: 'https://www.github.com/codeteenager' },
+        ],
+        sidebar:  {
+            '/web/': ['', 'web1']
+        },
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated',
+    }
+}
+
